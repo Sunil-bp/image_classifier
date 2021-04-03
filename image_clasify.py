c623202e2979554a6b31c1af25f4ae7965cdf08f
sunil <sunilbpoojari@gmail.com>
2021-04-03 09:49:37 +0530

    Testing database connection

--- image_clasify.py
+++ image_clasify.py
@@ -11,27 +11,29 @@ def recognize_text(img_path):
     '''loads an image and recognizes text.'''
 
     reader = easyocr.Reader(['en'])
-    return reader.readtext(img_path,paragraph=False)
+    return reader.readtext(img_path, paragraph=False)
+
 
 def get_files(dir_name):
-    file_list  = []
+    file_list = []
     for (dir_path, dir_names, dir_files) in os.walk(dir_name):
-        file_list += [os.path.join(dir_path,cur_files)
+        file_list += [os.path.join(dir_path, cur_files)
                       for cur_files in dir_files
-                      if re.search("[\w]+\.(?i)(jpe?g|png|gif|bmp)$",cur_files)]
+                      if re.search("[\w]+\.(?i)(jpe?g|png|gif|bmp)$", cur_files)]
     print(f"\n\n\nNumber of files {len(file_list)}")
     return file_list
+
 
 def main():
     print('Starting image processing ')
     print("Getting all image files  ")
     files = get_files(r"C:\Users\sunil\Desktop\meme_classifier\test_data")
-    final_data  = { }
-    for file  in files:
+    final_data = {}
+    for file in files:
         print(f"executing for file  {file} ")
-        data  = recognize_text(file)
+        data = recognize_text(file)
         print(data)
-        ait  = input("hello")
+        ait = input("hello")
 
 
 if __name__ == '__main__':
